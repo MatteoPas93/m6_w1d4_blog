c188ebf0b5ca103ce584f7e65513703a05aefea7
MatteoPas93 <pascucci-matteooutlook.it>
2024-03-25 18:55:16 +0100

    css main

--- frontend/src/components/Main/main.jsx
+++ frontend/src/components/Main/main.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./main.css";
 import UpdateCoverForm from "../Form/FormUpdateCover";
+// import { Link } from "react-router-dom";
 
 const Main = () => {
   const [posts, setPosts] = useState([]);
@@ -10,7 +11,9 @@ const Main = () => {
   const [selectedPost, setSelectedPost] = useState(null);
 
   const handlePostClick = (postId) => {
-    setSelectedPost(postId);
+    // setSelectedPost(postId);
+    setSelectedPost((prevSelectedPost) =>
+    prevSelectedPost === postId ? null : postId)
   };
   const fetchPosts = async () => {
     try {
@@ -41,37 +44,41 @@ const Main = () => {
     return <div> No posts available!</div>;
   }
   return (
-    <div className="container-main">
+    <div className="container-main ml-3">
       <div className="text-center">
         <h3> Total post: {posts.length} </h3>
       </div>
-      <div className="row justify-content-between">
-        <ul>
+      <div className="container-card row justify-content-between">
+       
           {posts.map((post) => {
             return (
               <div key={post._id} className="card g-2">
-                <li>
+               
                   <h5> {post.title} </h5>
                   <h6> {post.author.name} </h6>
                   <img src={post.cover} alt="img" />
                   <p>Categoria: {post.category} </p>
                   <a href={post.content}>Vai alla pagina dell'articolo</a>
                   <p> Tempo di lettura: {post.readTime} min </p>
-                  <button onClick={() => handlePostClick(post._id)}>
-                    Update Cover
-                  </button>
-                  {selectedPost === post._id && (
+                   <button onClick={() => handlePostClick(post._id)}>
+                    {selectedPost === post._id ? "Close window update" : "Update Cover"}
+                  </button> 
+                   {selectedPost === post._id && (
+                    <div className="container-form-update">
                     <UpdateCoverForm
                       postId={post._id}
                       onUpdateCover={handleCoverUpdate}
+                    /></div>
+                  )} 
 
-                    />
-                  )}
-                </li>
+                  {/* <Link to={`/updateCover/${post._id}`}>
+                  Update Cover
+                  </Link> */}
+                
               </div>
             );
           })}
-        </ul>
+        
       </div>
     </div>
   );
