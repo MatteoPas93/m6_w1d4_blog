7bb311bdc433a58df3de95008dd56126d4108bb2
MatteoPas93 <pascucci-matteooutlook.it>
2024-03-25 21:14:48 +0100

    paginazione

--- frontend/src/components/Main/main.jsx
+++ frontend/src/components/Main/main.jsx
@@ -4,23 +4,29 @@ import axios from "axios";
 import "./main.css";
 import UpdateCoverForm from "../Form/FormUpdateCover";
 // import { Link } from "react-router-dom";
+import ResponsivePagination from "react-responsive-pagination";
+import "react-responsive-pagination/themes/classic.css";
 
 const Main = () => {
   const [posts, setPosts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [selectedPost, setSelectedPost] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(4);
+  const [totalPages, setTotalPages] = useState(0);
 
   const handlePostClick = (postId) => {
-    // setSelectedPost(postId);
     setSelectedPost((prevSelectedPost) =>
-    prevSelectedPost === postId ? null : postId)
+      prevSelectedPost === postId ? null : postId
+    );
   };
   const fetchPosts = async () => {
     try {
       const response = await axios.get(
-        `${process.env.REACT_APP_SERVER_BASE_URL}/getPosts`
+        `${process.env.REACT_APP_SERVER_BASE_URL}/getPosts?page=${currentPage}&pageSize=${pageSize}`
       );
       setPosts(response.data.posts);
+      setTotalPages(response.data.totalPages);
       setIsLoading(false);
     } catch (error) {
       console.error("Error get posts", error);
@@ -30,7 +36,7 @@ const Main = () => {
 
   useEffect(() => {
     fetchPosts();
-  }, []);
+  }, [currentPage, pageSize]);
 
   const handleCoverUpdate = async () => {
     await fetchPosts();
@@ -43,44 +49,51 @@ const Main = () => {
   if (!Array.isArray(posts) || posts.length === 0) {
     return <div> No posts available!</div>;
   }
-  return (
+  return (<>
+              <ResponsivePagination 
+              current= {currentPage}
+              total = {totalPages}
+              onPageChange={setCurrentPage}
+              />
     <div className="container-main ml-3">
       <div className="text-center">
         <h3> Total post: {posts.length} </h3>
       </div>
       <div className="container-card row justify-content-between">
-       
-          {posts.map((post) => {
-            return (
+        {posts.map((post) => {
+          return (
+            
               <div key={post._id} className="card g-2">
-               
-                  <h5> {post.title} </h5>
-                  <h6> {post.author.name} </h6>
-                  <img src={post.cover} alt="img" />
-                  <p>Categoria: {post.category} </p>
-                  <a href={post.content}>Vai alla pagina dell'articolo</a>
-                  <p> Tempo di lettura: {post.readTime} min </p>
-                   <button onClick={() => handlePostClick(post._id)}>
-                    {selectedPost === post._id ? "Close window update" : "Update Cover"}
-                  </button> 
-                   {selectedPost === post._id && (
-                    <div className="container-form-update">
+                <h5> {post.title} </h5>
+                <h6> {post.author.name} </h6>
+                <img src={post.cover} alt="img" />
+                <p>Categoria: {post.category} </p>
+                <a href={post.content}>Vai alla pagina dell'articolo</a>
+                <p> Tempo di lettura: {post.readTime} min </p>
+                <button onClick={() => handlePostClick(post._id)}>
+                  {selectedPost === post._id
+                    ? "Close window update"
+                    : "Update Cover"}
+                </button>
+                {selectedPost === post._id && (
+                  <div className="container-form-update">
                     <UpdateCoverForm
                       postId={post._id}
                       onUpdateCover={handleCoverUpdate}
-                    /></div>
-                  )} 
+                    />
+                  </div>
+                )}
 
-                  {/* <Link to={`/updateCover/${post._id}`}>
+                {/* <Link to={`/updateCover/${post._id}`}>
                   Update Cover
                   </Link> */}
-                
               </div>
-            );
-          })}
-        
+            
+          );
+        })}
       </div>
     </div>
+    </>
   );
 };
 
